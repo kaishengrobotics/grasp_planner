7e3d816d63a7bfe10917571ba951e8d492346b26
Qingkai Lu <luqingkai@gmail.com>
2021-12-26 21:14:02 -0800

    Compute the grasping position of the PCA-based grasp planner.

---
+++ include/grasp_planners/pca_grasp_planner.h
@@ -0,0 +1,53 @@
+#ifndef PCA_GRASP_PLANNER_H
+#define PCA_GRASP_PLANNER_H
+
+#include <pcl/point_types.h>
+
+/**
+* The definition of a bounding box.
+*/
+struct BoundingBox{
+    // The pose of the bounding box center.
+    geometry_msgs::PoseStamped& center_pose;
+    // The rotation matrix of the center orientation.
+    Eigen::Matrix3f center_ort_mat;
+    // The bounding box size. The first to third value corresponds to the 
+    // first to third principal component range.
+    Eigen::Vector3f size;
+};
+
+/**
+* A PCA-based grasp planner.
+*/
+class PCAGraspPlanner{
+    public:
+        PCAGraspPlanner(bool vis_bb=false): vis_bounding_box_(vis_bb) {};
+
+        // TODO: transform the point cloud to make sure the z axis is the 
+        // gravity direction.
+
+        /**
+        * Compute the bounding box associated with an object cloud using PCA.
+        *
+        * \param obj_cloud The object point cloud.
+        * \param bounding_box [output] The object bounding box.
+        */
+        void findCloudBoundingBoxPCA(
+                            pcl::PointCloud<pcl::PointXYZRGB>::Ptr& obj_cloud,
+                            BoundingBox& bb);
+        /**
+        * Generate a heuristic overhead grasp using the object bounding box.
+        * We assume the z axis of the bounding box is along the gravity direction.
+        * \param bb The bounding box.
+        * \param grasp_pose [output] The grasping pose.
+        */
+        void genOverheadGraspPoseBB(const BoundingBox& bb, 
+                                    geometry_msgs::Pose &grasp_pose);
+        
+    private:
+        // Visualize the bounding box or not.
+        bool vis_bounding_box_;
+
+};
+
+#endif
--- src/pca_grasp_planner.cpp
+++ src/pca_grasp_planner.cpp
@@ -11,22 +11,12 @@
 #include <math.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/passthrough.h>
-#include <pca_grasp_planner.h>
-
-// typedef sensor_msgs::PointCloud2 MsgCloud;
-// typedef pcl::PointXYZRGB pcl::PointCloud<pcl::PointXYZ>RGB;
-// typedef pcl::PointCloud<pcl::PointCloud<pcl::PointXYZ>RGB> PCXYZRGB;
-// typedef pcl::Normal PtNormals;
-// typedef pcl::PointCloud<PtNormals> PCNormals;
-// typedef pcl::PointXYZ pcl::PointCloud<pcl::PointXYZ>;
-// typedef pcl::PointCloud<pcl::PointCloud<pcl::PointXYZ>> PCXYZ;
-// typedef pcl::search::KdTree<pcl::PointCloud<pcl::PointXYZ>RGB>::Ptr KdTreePtr;
-// using boost::shared_ptr;
+#include <tf/tf.h>
+#include <grasp_planners/pca_grasp_planner.h>
 
 void PCAGraspPlanner::findCloudBoundingBoxPCA(
-                            pcl::PointCloud<pcl::PointXYZRGB>::Ptr& obj_cloud, 
-                            geometry_msgs::Pose& pose, double& width, 
-                            double& height, double& depth)
+                            pcl::PointCloud<pcl::PointXYZRGB>::Ptr& obj_cloud,
+                            BoundingBox& bb);
 {
     Eigen::Vector3f eigen_values;
     Eigen::Matrix3f eigen_vectors;
@@ -37,11 +27,8 @@ void PCAGraspPlanner::findCloudBoundingBoxPCA(
     pcl::PCA<pcl::PointCloud<pcl::PointXYZ>> pca;
     try{
       pca.setInputCloud(obj_cloud.makeShared());
-      ROS_DEBUG_STREAM("Getting mean");
       centroid = pca.getMean();
-      ROS_DEBUG_STREAM("Getting eigen values");
       eigen_values = pca.getEigenValues();
-      ROS_DEBUG_STREAM("Getting eigen vectors");
       eigen_vectors = pca.getEigenVectors();
     } catch(pcl::InitFailedException ife)
     {
@@ -54,16 +41,16 @@ void PCAGraspPlanner::findCloudBoundingBoxPCA(
     proj_transform.block<3,3>(0,0) = eigen_vectors.transpose();
     proj_transform.block<3,1>(0,3) = -1.f * (proj_transform.block<3,3>(0,0) * centroid.head<3>());
     // Transform points into eigen space to find extrema and compute ranges
-    PCXYZ proj;
+    pcl::PointCloud<pcl::PointXYZ> proj;
     // pca.project(obj_cloud, proj);
     pcl::transformPointCloud(obj_cloud, proj, proj_transform);
     
     pcl::PointCloud<pcl::PointXYZ> proj_min;
     pcl::PointCloud<pcl::PointXYZ> proj_max;
     pcl::getMinMax3D(proj, proj_min, proj_max);
-    width = fabs(proj_max.x-proj_min.x);
-    height = fabs(proj_max.y-proj_min.y);
-    depth = fabs(proj_max.z-proj_min.z);
+    bb.size(0) = fabs(proj_max.x-proj_min.x);
+    bb.size(1) = fabs(proj_max.y-proj_min.y);
+    bb.size(2) = fabs(proj_max.z-proj_min.z);
     
     Eigen::Vector3f box_mean_eigenspace = 0.5f*(proj_max.getVector3fMap() +
                                                 proj_min.getVector3fMap());
@@ -73,19 +60,20 @@ void PCAGraspPlanner::findCloudBoundingBoxPCA(
     orientation.normalize();
     Eigen::Vector3f rectified_centroid = eigen_vectors*box_mean_eigenspace + \
                                          centroid.head<3>();
-    pose.position.x = rectified_centroid[0];
-    pose.position.y = rectified_centroid[1];
-    pose.position.z = rectified_centroid[2];
-    pose.orientation.x = orientation.x();
-    pose.orientation.y = orientation.y();
-    pose.orientation.z = orientation.z();
-    pose.orientation.w = orientation.w();
-    
+    bb.center_pose.pose.position.x = rectified_centroid[0];
+    bb.center_pose.pose.position.y = rectified_centroid[1];
+    bb.center_pose.pose.position.z = rectified_centroid[2];
+    bb.center_pose.pose.orientation.x = orientation.x();
+    bb.center_pose.pose.orientation.y = orientation.y();
+    bb.center_pose.pose.orientation.z = orientation.z();
+    bb.center_pose.pose.orientation.w = orientation.w();
+    bb.center_pose.header.frame_id = obj_cloud->header.frame_id; 
+    bb.center_ort_mat = eigen_vectors;
+   
     if (debug_)
-      ROS_INFO_STREAM("width = " << width << "\theight = " << height <<
-                      "\tdepth = " << depth);
+      ROS_INFO_STREAM("BB size: " << bb.size);
     
-    if (visualize_bounding_box_)
+    if (vis_bounding_box_)
     {
       ROS_INFO_STREAM("eigen values:\n" << eigen_values);
       ROS_INFO_STREAM("eigen vectors\n" << eigen_vectors);
@@ -93,7 +81,7 @@ void PCAGraspPlanner::findCloudBoundingBoxPCA(
       ROS_INFO_STREAM("pca centroid:\n" << centroid);
       ROS_INFO_STREAM("box_mean:\n" << box_mean_eigenspace);
       ROS_INFO_STREAM("Recovered centroid:\n" << rectified_centroid);
-      ROS_INFO_STREAM("Recovered orientation:\n"<< pose.orientation);
+      ROS_INFO_STREAM("Recovered orientation:\n"<< bb.center_pose.orientation);
       /* visualizes the inputed pointcloud */
       pcl::visualization::PCLVisualizer viewer("PCL Viewer");
       viewer.setBackgroundColor(0.0,0.0,0.7);
@@ -102,7 +90,7 @@ void PCAGraspPlanner::findCloudBoundingBoxPCA(
       viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2,
                                               "object_cloud");
     
-      viewer.addCube(rectified_centroid, orientation, width, height, depth);
+      viewer.addCube(rectified_centroid, orientation, bb.size(0), bb.size(1), bb.size(2));
       viewer.setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION,
     	                         pcl::visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME,
     				 "cube");
@@ -113,4 +101,44 @@ void PCAGraspPlanner::findCloudBoundingBoxPCA(
 }
 }
 
+void PCAGraspPlanner::genOverheadGraspPoseBB(const BoundingBox& bb, 
+                            geometry_msgs::PoseStamped& grasp_pose)
+{
+    // Find the axis whose angle between itself and the z axis 
+    // (grativity direction) is the smallest. 
+    double max_z = fabs(bb.center_ort_mat(2, 0));
+    int max_z_index = 0;
+    for(int i = 1; i < 3; ++i) 
+    {
+        double z_val = fabs(bb.center_ort_mat(2, i));
+        if(z_val > max_z)
+        {
+            max_z = z_val;
+            max_z_index = i;
+        }
+    }
+    
+    // Compute the position of the overhead grasp.
+    Eigen::Vector3f z_vec = bb.center_ort_mat.col(max_z_index);
+    // Make sure the grasp position is at the top instead of bottom. 
+    if(z_vec(2) < 0.)
+        z_vec *= -1;
+    z_vec *= bb.size(max_z_index);
+    grasp_pose.pose.position.x = bb.center_pose.position.x + z_vec(0); 
+    grasp_pose.pose.position.y = bb.center_pose.position.y + z_vec(1); 
+    grasp_pose.pose.position.z = bb.center_pose.position.z + z_vec(2);
+
+    // Compute the orientation of the overhead grasp.
+    // We assume the x axis of the gripper palm/grasping link points
+    // from the palm to the middle of the two finger tips.
+    // The y axis points from the right finger to the left.
+    Eigen::Matrix3f grasp_ort_mat;
+    grasp_ort_mat.col(0) = -1. * z_vec;
+    // Among the other two boudning box orientation axis, we select 
+    // the one with a smaller size/eigenvalues to be the y axis. 
+    // This means the robot grasps the thinner side of the object.
+
+    grasp_pose.header.frame_id = bb.center_pose.header.frame_id;
+    return;
+}
 
